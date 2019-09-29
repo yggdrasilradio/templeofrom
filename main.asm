69ba3636e6f1c1a617f609844d1647e995a62c65
Rick Adams <webmaster@rickadams.org>
2019-09-28 20:44:46 -0500

    Comment portal code

--- main.asm
+++ main.asm
@@ -2728,11 +2728,16 @@ LDF1A	fdb %0000010101000000
 ; Y coordinate (16 bits)
 ; 4 sets of destination coordinates (in packed divided by four format)
 ;   one of which is chosen at "random" when a portal is activated
-LDF2A	fcb $01,$b0,$01,$54,$0b,$0b,$ba,$09,$13,$48,$aa,$89
-	fcb $00,$2c,$00,$2c,$6c,$55,$ba,$09,$13,$48,$aa,$89
-	fcb $02,$e8,$00,$24,$0b,$0b,$6c,$55,$13,$48,$aa,$89
-	fcb $00,$4c,$01,$20,$0b,$0b,$ba,$09,$6c,$55,$aa,$89
-	fcb $02,$a8,$02,$24,$0b,$0b,$ba,$09,$13,$48,$6c,$55
+LDF2A	fcb $01,$b0,$01,$54			; PORTAL 1 6c55
+	fcb $0b,$0b,$ba,$09,$13,$48,$aa,$89	; 	PORTAL 2 PORTAL 3 PORTAL 4 PORTAL 5
+	fcb $00,$2c,$00,$2c			; PORTAL 2 0b0b
+	fcb $6c,$55,$ba,$09,$13,$48,$aa,$89	;	PORTAL 1 PORTAL 3 PORTAL 4 PORTAL 5 
+	fcb $02,$e8,$00,$24			; PORTAL 3 ba09
+	fcb $0b,$0b,$6c,$55,$13,$48,$aa,$89	;	PORTAL 2 PORTAL 1 PORTAL 4 PORTAL 5 
+	fcb $00,$4c,$01,$20			; PORTAL 4 1348
+	fcb $0b,$0b,$ba,$09,$6c,$55,$aa,$89	;	PORTAL 2 PORTAL 3 PORTAL 1 PORTAL 5 
+	fcb $02,$a8,$02,$24			; PORTAL 5 aa89
+	fcb $0b,$0b,$ba,$09,$13,$48,$6c,$55	;	PORTAL 2 PORTAL 3 PORTAL 4 PORTAL 1 
 	fdb 0			; flag end of table
 
 drawmazeboth lbsr clearrender	; get a clean render area
