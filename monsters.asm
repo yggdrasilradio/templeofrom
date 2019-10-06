6bf8d263a18cd77b052b1ad012cb550bea62297c
Rick Adams <webmaster@rickadams.org>
2019-10-06 13:38:16 -0500

    Add spider

--- monsters.asm
+++ monsters.asm
@@ -7,15 +7,15 @@
 ; Coordinates are 16 bits divided by 4 and stored in 8 bits
 
 ; x 385 to 477, $60 to $77 +1 = $61, $78 spider at center bottom of maze
-; y 697 to 749, $ae to $bb +2 = $b0, $bd
+; y 701 to 753, $AF to $BC +1 = $B0, $BD
 
 ; x 385 to 477, $60 to $77 +1 = $61, $78 new spider
-; y 621 to 673, $9B to $a8 +2 = $9d, $aa
+; y 622 to 674, $9C to $A9 +1 = $9D, $AA
 
 LDD23
 	fcb $57,$65,$63,$75,$00
-	fcb $03,$10,$02,$10,$20
-	fcb $20,$2d,$02,$10,$20
+	fcb $03,$10,$02,$10,$20 ; fireball in queen's bedroom
+	fcb $20,$2d,$02,$10,$20 ; fireball in king's bedroom
 	fcb $28,$32,$18,$32,$00
 	fcb $52,$68,$03,$11,$00
 	fcb $6d,$80,$0b,$23,$20
@@ -30,9 +30,9 @@ LDD23
 	fcb $c1,$cb,$4d,$67,$00
 	fcb $30,$41,$4a,$5f,$20
 	fcb $0d,$23,$55,$60,$00
-	;fcb $61,$78,$9d,$aa,$00 ; <-- NEW spider in room just above next room
+	fcb $61,$78,$9d,$aa,$00 ; <-- NEW spider in room just above next room
 	fcb $61,$78,$b0,$bd,$00 ; <-- spider at center bottom of maze
-	fcb $b8,$d4,$83,$a5,$20
+	fcb $b8,$d4,$83,$a5,$20 ; <-- fireball at lower right
 	fcb $00,$00		; mark end of table
 
 LDD84	fcb $20,$08,$08,$20,$03,$c0,$ab,$ea	; 2 spider sprites 8x8
