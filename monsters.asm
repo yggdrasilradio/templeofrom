c85c0b0225cef63c816a1a162d179a292bed32bd
Rick Adams <webmaster@rickadams.org>
2019-10-05 19:26:32 -0500

    Figure out how to add a monster

--- monsters.asm
+++ monsters.asm
@@ -1,9 +1,17 @@
 ; Monster location table (19 x 5 + 2 = 97 bytes)
 ; X1, X2, Y1, Y2, monster ID (00 = spider, 20 = fireball)
 ; (X1, Y1) and (X2, Y2) are the corners of a box
-; The monster's home position is at the center of the box
+; The monster's home position is 1/4 down from the top edge of the box
+; and 1/4 from the left edge of the box
 ; Players entering the box aggro the monster
 ; Coordinates are 16 bits divided by 4 and stored in 8 bits
+
+; x 385 to 477, $60 to $77 +1 = $61, $78 spider at center bottom of maze
+; y 697 to 749, $ae to $bb +2 = $b0, $bd
+
+; x 385 to 477, $60 to $77 +1 = $61, $78 new spider
+; y 621 to 673, $9B to $a8 +2 = $9d, $aa
+
 LDD23
 	fcb $57,$65,$63,$75,$00
 	fcb $03,$10,$02,$10,$20
@@ -22,7 +30,8 @@ LDD23
 	fcb $c1,$cb,$4d,$67,$00
 	fcb $30,$41,$4a,$5f,$20
 	fcb $0d,$23,$55,$60,$00
-	fcb $61,$78,$b0,$bd,$00
+	;fcb $61,$78,$9d,$aa,$00 ; <-- NEW spider in room just above next room
+	fcb $61,$78,$b0,$bd,$00 ; <-- spider at center bottom of maze
 	fcb $b8,$d4,$83,$a5,$20
 	fcb $00,$00		; mark end of table
 
